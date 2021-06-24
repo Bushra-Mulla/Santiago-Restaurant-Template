68ffdc135977e197631e97ef8e88d55d6021f636
bushra-madinah-llc <bushra@madinah.llc>
2021-06-24 14:14:32 +0300

    updet template

--- js/main.js
+++ js/main.js
@@ -63,25 +63,13 @@ finishBtn.addEventListener("click", () => {
 });
 
 // slider
-
-$('#carouselExample').on('slide.bs.carousel', function (e) {
-
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 4;
-    var totalItems = $('.carousel-item').length;
-    
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
+$(".carousel").flickity({
+  // options
+  cellAlign: "left",
+  contain: true,
 });
-
+$(".main-carousel").flickity({
+  // options
+  cellAlign: "left",
+  contain: true,
+});
